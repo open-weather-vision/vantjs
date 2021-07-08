fa0a3cbeb3151da100966336a64ab68e37f6b9a5
harrydehix <harry.holly02@gmail.com>
2021-07-08 13:39:40 +0200

    Complete parsing of LOOP package

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 import inspect from "./util/inspect";
 import VantageInterface from "./VantageInterface";
 
-
+// The interface automatically connects to the console in the background and tries to wake it up.
 const device = new VantageInterface("COM3");
 
+// Once the console has been woken up, you can interact with it.
 device.once("awakening", async () => {
     console.log("Connected to device!");
 
+    // You always should validate the connection
     if (await device.validateConnection()) {
         console.log("Test worked!")
     } else {
-        console.log("Test failed!")
+        throw new Error("Connection to console failed.");
     }
 
+    // Getting the console's firmware version
+    const firmwareVersion = await device.getFirmwareVersion();
+    console.log(firmwareVersion);
 
-    // const result = await device.getFirmwareVersion();
-    // console.log(result);
+    // Getting the current highs and lows values
+    const highsAndLows = await device.getHighsAndLows();
+    console.log(highsAndLows);
 
-    // const highsAndLows = await device.getHighsAndLows();
-    // inspect(highsAndLows);
-
+    // Getting the currently measured weather data
     const realtimeData = await device.getRealtimeData();
-    inspect(realtimeData);
+    console.log(realtimeData);
 
+    // Closing the connection to the console
     device.close();
 })
